c6dd9e3048995d7898cd7ba89da3e428ee8a00b3
Mikhail Zaks <mikhail_zaks@epam.com>
2022-03-08 14:19:22 +0300

    add integration test

---
+++ src/test/java/com/epam/system_monitoring/integration/course/get/GetRequestTest.java
@@ -0,0 +1,104 @@
+package com.epam.system_monitoring.integration.course.get;
+
+import com.epam.system_monitoring.entity.Course;
+import com.epam.system_monitoring.entity.Module;
+import com.epam.system_monitoring.repository.CourseRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class GetRequestTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CourseRepository courseRepository;
+
+    @BeforeEach
+    public void setUp() {
+        List<Module> moduleList = Arrays.asList(
+                new Module(1L, "Module 1", null, null, null, null, null, null),
+                new Module(2L, "Module 2", null, null, null, null, null, null)
+        );
+        Course course = new Course(1L, "Course 1", moduleList);
+
+        when(courseRepository.findByTitle("Course 1")).thenReturn(Optional.of(course));
+        when(courseRepository.findById(1L)).thenReturn(Optional.of(course));
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void shouldReturnAllCourses() throws Exception {
+        List<Course> courseList = Arrays.asList(
+                new Course(1L, "Course 1", List.of()),
+                new Course(2L, "Course 2", List.of()));
+
+        when(courseRepository.findAll()).thenReturn(courseList);
+        mockMvc.perform(get("http://localhost:8080/api/course/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.courses").isArray())
+                .andExpect(jsonPath("$.courses", hasSize(2)))
+                .andExpect(jsonPath("$.courses[0].title", is("Course 1")))
+                .andExpect(jsonPath("$.courses[1].title", is("Course 2")));
+    }
+
+    @Test
+    public void shouldReturnCourseById() throws Exception {
+        mockMvc.perform(get("http://localhost:8080/api/course/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", is("Course 1")))
+                .andExpect(jsonPath("$.modules").isArray())
+                .andExpect(jsonPath("$.modules", hasSize(2)))
+                .andExpect(jsonPath("$.modules[0].title", is("Module 1")))
+                .andExpect(jsonPath("$.modules[1].title", is("Module 2")));
+    }
+
+    @Test
+    public void shouldReturnCourseByTitle() throws Exception {
+        String title = "Course 1";
+        mockMvc.perform(get("http://localhost:8080/api/course/title/" + title))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", is("Course 1")));
+    }
+
+    @Test
+    public void shouldReturnAllModulesInCourse() throws Exception {
+        mockMvc.perform(get("http://localhost:8080/api/course/1/modules"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].title", is("Module 1")))
+                .andExpect(jsonPath("$[1].title", is("Module 2")));
+    }
+}
---
+++ src/test/java/com/epam/system_monitoring/integration/course/post/PostRequestTest.java
@@ -0,0 +1,140 @@
+package com.epam.system_monitoring.integration.course.post;
+
+import com.epam.system_monitoring.dto.CourseDTO;
+import com.epam.system_monitoring.entity.Course;
+import com.epam.system_monitoring.mappers.CourseMapper;
+import com.epam.system_monitoring.repository.CourseRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class PostRequestTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CourseRepository courseRepository;
+
+    private static Course course;
+
+    @BeforeEach
+    public void setUp() {
+        course = new Course(1L, "Course 1", List.of());
+
+        when(courseRepository.findById(1L)).thenReturn(Optional.of(course));
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void shouldCreateNewCourse() throws Exception {
+        CourseDTO courseDTO = new CourseDTO("Test title", List.of());
+        Course newCourse = CourseMapper.INSTANCE.toCourse(courseDTO);
+        when(courseRepository.save(newCourse)).thenReturn(newCourse);
+
+        mockMvc.perform(post("http://localhost:8080/api/course/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(courseDTO))
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title", is("Test title")))
+                .andExpect(jsonPath("$.modules").isArray())
+                .andExpect(jsonPath("$.modules", hasSize(0)))
+                .andReturn();
+
+        verify(courseRepository).findByTitle(courseDTO.getTitle());
+        verify(courseRepository).save(newCourse);
+    }
+
+    @Test
+    public void shouldUpdateCourse() throws Exception {
+        CourseDTO courseDTO = new CourseDTO("Updated title", List.of());
+        Course updatedCourse = CourseMapper.INSTANCE.toCourse(courseDTO);
+        updatedCourse.setId(course.getId());
+
+        when(courseRepository.existsByTitle(courseDTO.getTitle())).thenReturn(false);
+        when(courseRepository.save(updatedCourse)).thenReturn(updatedCourse);
+
+        mockMvc.perform(post("http://localhost:8080/api/course/update/" + course.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(courseDTO))
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", is("Updated title")))
+                .andExpect(jsonPath("$.modules").isArray())
+                .andExpect(jsonPath("$.modules", hasSize(0)))
+                .andReturn();
+
+        verify(courseRepository).existsByTitle(courseDTO.getTitle());
+        verify(courseRepository).findById(course.getId());
+        verify(courseRepository).save(updatedCourse);
+    }
+
+    @Test
+    public void willThrowTitleAlreadyExistExceptionWhenUpdateCourse() throws Exception {
+        CourseDTO courseDTO = new CourseDTO("Course 1", List.of());
+        Course testCourse = CourseMapper.INSTANCE.toCourse(courseDTO);
+        course.setId(course.getId());
+
+        when(courseRepository.existsByTitle(courseDTO.getTitle())).thenReturn(true);
+        mockMvc.perform(post("http://localhost:8080/api/course/update/" + course.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(courseDTO))
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", is("A course with this title already exists")))
+                .andReturn();
+
+        verify(courseRepository, never()).save(testCourse);
+    }
+
+    @Test
+    public void shouldDeleteCourse() throws Exception {
+        doNothing().when(courseRepository).delete(course);
+
+        mockMvc.perform(post("http://localhost:8080/api/course/delete/" + course.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(String.format("Course with id: %d was deleted", course.getId())))
+                .andReturn();
+    }
+
+    private static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
