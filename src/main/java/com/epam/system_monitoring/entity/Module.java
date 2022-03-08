b5386827830d41b18d4793fdd5d59b2d09b9d347
Mikhail Zaks <mikhail_zaks@epam.com>
2022-03-08 14:19:48 +0300

    add @AllArgsConstructor

--- src/main/java/com/epam/system_monitoring/entity/Module.java
+++ src/main/java/com/epam/system_monitoring/entity/Module.java
@@ -1,5 +1,6 @@
 package com.epam.system_monitoring.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import java.time.LocalTime;
 
 @Entity
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "module")
 public class Module {
